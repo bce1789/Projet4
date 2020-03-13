d97d985fd61eeafa85716e675b488d6729503112
Benoit Coste <bce.1789@gmail.com>
2020-03-13 13:50:30 +0100

    update back

--- billets/admin.php
+++ billets/admin.php
@@ -2,135 +2,133 @@
 require 'autoload.php';
 require 'NewsManager.php';
 ?>
+
 <head>
-    <?php require_once('../inc/headScript.php');?>
-  </head>
-  
-  <body>
-    <?php require_once '../inc/header.php'; ?>
-    <div class="mx-auto" style="width: 50px;">
-      <!--Espace vide -->
-      <p></p>
-    </div>
-    
-    <div class="row">
-    <?php require_once('../inc/menu.php');?>
-      <div class="col-md-8 blog-main">
-        <h3 class="pb-4 mb-4 font-italic border-bottom">
-          Publications
-        </h3>
-<?php
-$db = DBFactory::getMysqlConnexionWithPDO();
-$manager = new NewsManagerPDO($db);
+  <?php require_once('../inc/headScript.php'); ?>
+</head>
 
-if (isset($_GET['modifier']))
-{
-  $news = $manager->getUnique((int) $_GET['modifier']);
-}
+<body>
+  <?php require_once '../inc/header.php'; ?>
+  <div class="mx-auto" style="width: 50px;">
+    <!--Espace vide -->
+    <p></p>
+  </div>
 
-if (isset($_GET['supprimer']))
-{
-  $manager->delete((int) $_GET['supprimer']);
-  $message = 'La news a bien été supprimée !';
-}
+  <div class="row">
+    <?php require_once('../inc/menu.php'); ?>
+    <div class="col-md-8 blog-main">
+      <h3 class="pb-4 mb-4 font-italic border-bottom">
+        Publications
+      </h3>
+      <?php
+      $db = DBFactory::getMysqlConnexionWithPDO();
+      $manager = new NewsManagerPDO($db);
 
-if (isset($_POST['auteur']))
-{
-  $news = new News(
-    [
-      'auteur' => $_POST['auteur'],
-      'titre' => $_POST['titre'],
-      'contenu' => $_POST['contenu']
-    ]
-  );
-  
-  if (isset($_POST['id']))
-  {
-    $news->setId($_POST['id']);
-  }
-  
-  if ($news->isValid())
-  {
-    $manager->save($news);
-    
-    $message = $news->isNew() ? 'La news a bien été ajoutée !' : 'La news a bien été modifiée !';
-  }
-  else
-  {
-    $erreurs = $news->erreurs();
-  }
-}
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Administration</title>
-    <meta charset="utf-8" />
-    
-    <style type="text/css">
-      table, td {
-        border: 1px solid black;
+      if (isset($_GET['modifier'])) {
+        $news = $manager->getUnique((int) $_GET['modifier']);
       }
-      
-      table {
-        margin:auto;
-        text-align: center;
-        border-collapse: collapse;
+
+      if (isset($_GET['supprimer'])) {
+        $manager->delete((int) $_GET['supprimer']);
+        $message = 'La news a bien été supprimée !';
       }
-      
-      td {
-        padding: 3px;
+
+      if (isset($_POST['auteur'])) {
+        $news = new News(
+          [
+            'auteur' => $_POST['auteur'],
+            'titre' => $_POST['titre'],
+            'contenu' => $_POST['contenu']
+          ]
+        );
+
+        if (isset($_POST['id'])) {
+          $news->setId($_POST['id']);
+        }
+
+        if ($news->isValid()) {
+          $manager->save($news);
+
+          $message = $news->isNew() ? 'La news a bien été ajoutée !' : 'La news a bien été modifiée !';
+        } else {
+          $erreurs = $news->erreurs();
+        }
       }
-    </style>
-  </head>
-  
-  <body>
-    <p><a href="/p4_coste_benoit/billets/billet.php">Accéder aux publications</a></p>
-    
-    <form action="admin.php" method="post">
-      <p style="text-align: center">
-<?php
-if (isset($message))
-{
-  echo $message, '<br />';
-}
-?>
-        <?php if (isset($erreurs) && in_array(News::AUTEUR_INVALIDE, $erreurs)) echo 'L\'auteur est invalide.<br />'; ?>
-        Auteur : <input type="text" name="auteur" value="<?php if (isset($news)) echo $news->auteur(); ?>" /><br />
-        
-        <?php if (isset($erreurs) && in_array(News::TITRE_INVALIDE, $erreurs)) echo 'Le titre est invalide.<br />'; ?>
-        Titre : <input type="text" name="titre" value="<?php if (isset($news)) echo $news->titre(); ?>" /><br />
-        
-        <?php if (isset($erreurs) && in_array(News::CONTENU_INVALIDE, $erreurs)) echo 'Le contenu est invalide.<br />'; ?>
-        Contenu :<br /><textarea rows="8" cols="60" name="contenu"><?php if (isset($news)) echo $news->contenu(); ?></textarea><br />
-<?php
-if(isset($news) && !$news->isNew())
-{
-?>
-        <input type="hidden" name="id" value="<?= $news->id() ?>" />
-        <input type="submit" value="Modifier" name="modifier" />
-<?php
-}
-else
-{
-?>
-        <input type="submit" value="Ajouter" />
-<?php
-}
-?>
-      </p>
-    </form>
-    
-    <p style="text-align: center">Il y a actuellement <?= $manager->count() ?> publications. En voici la liste :</p>
-    
-    <table>
-      <tr><th>Auteur</th><th>Titre</th><th>Date d'ajout</th><th>Dernière modification</th><th>Action</th></tr>
-<?php
-foreach ($manager->getList() as $news)
-{
-  echo '<tr><td>', $news->auteur(), '</td><td>', $news->titre(), '</td><td>', $news->dateAjout()->format('d/m/Y à H\hi'), '</td><td>', ($news->dateAjout() == $news->dateModif() ? '-' : $news->dateModif()->format('d/m/Y à H\hi')), '</td><td><a href="?modifier=', $news->id(), '">Modifier</a> | <a href="?supprimer=', $news->id(), '">Supprimer</a></td></tr>', "\n";
-}
-?>
-    </table>
-  </body>
-</html>
+      ?>
+      <!DOCTYPE html>
+      <html>
+
+      <head>
+        <title>Administration</title>
+        <meta charset="utf-8" />
+
+        <style type="text/css">
+          table,
+          td {
+            border: 1px solid black;
+          }
+
+          table {
+            margin: auto;
+            text-align: center;
+            border-collapse: collapse;
+          }
+
+          td {
+            padding: 3px;
+          }
+        </style>
+      </head>
+
+      <body>
+        <p><a href="/p4_coste_benoit/billets/billet.php">Accéder aux publications</a></p>
+
+        <form action="admin.php" method="post">
+          <p style="text-align: center">
+            <?php
+            if (isset($message)) {
+              echo $message, '<br />';
+            }
+            ?>
+            <?php if (isset($erreurs) && in_array(News::AUTEUR_INVALIDE, $erreurs)) echo 'L\'auteur est invalide.<br />'; ?>
+            Auteur : <input type="text" name="auteur" value="<?php if (isset($news)) echo $news->auteur(); ?>" /><br />
+
+            <?php if (isset($erreurs) && in_array(News::TITRE_INVALIDE, $erreurs)) echo 'Le titre est invalide.<br />'; ?>
+            Titre : <input type="text" name="titre" value="<?php if (isset($news)) echo $news->titre(); ?>" /><br />
+
+            <?php if (isset($erreurs) && in_array(News::CONTENU_INVALIDE, $erreurs)) echo 'Le contenu est invalide.<br />'; ?>
+            Contenu :<br /><textarea rows="8" cols="60" name="contenu"><?php if (isset($news)) echo $news->contenu(); ?></textarea><br />
+            <?php
+            if (isset($news) && !$news->isNew()) {
+            ?>
+              <input type="hidden" name="id" value="<?= $news->id() ?>" />
+              <input type="submit" value="Modifier" name="modifier" />
+            <?php
+            } else {
+            ?>
+              <input type="submit" value="Ajouter" />
+            <?php
+            }
+            ?>
+          </p>
+        </form>
+
+        <p style="text-align: center">Il y a actuellement <?= $manager->count() ?> publications. En voici la liste :</p>
+
+        <table>
+          <tr>
+            <th>Auteur</th>
+            <th>Titre</th>
+            <th>Date d'ajout</th>
+            <th>Dernière modification</th>
+            <th>Action</th>
+          </tr>
+          <?php
+          foreach ($manager->getList() as $news) {
+            echo '<tr><td>', $news->auteur(), '</td><td>', $news->titre(), '</td><td>', $news->dateAjout()->format('d/m/Y à H\hi'), '</td><td>', ($news->dateAjout() == $news->dateModif() ? '-' : $news->dateModif()->format('d/m/Y à H\hi')), '</td><td><a href="?modifier=', $news->id(), '">Modifier</a> | <a href="?supprimer=', $news->id(), '">Supprimer</a></td></tr>', "\n";
+          }
+          ?>
+        </table>
+      </body>
+
+      </html>
