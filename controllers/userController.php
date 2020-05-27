ee58ac21c68c39583582119b30cc4af7cdbd7a2c
Benoit Coste <bce.1789@gmail.com>
2020-05-27 14:03:34 +0200

    resolve login issue, add flash message

--- controllers/userController.php
+++ controllers/userController.php
@@ -11,11 +11,15 @@ class userController
 
             if (empty($_POST['password']) || $_POST['password'] != $_POST['password_confirm']) {
                 $_SESSION['flash']['danger'] = "Les mots de passes ne correspondent pas";
+                
             } else {
                 $user_id = $_SESSION['auth']->id;
                 $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+                $userModel = new userModel;
+                $userModel->user($password, $user_id);
                 $_SESSION['flash']['success'] = "Votre mot de passe a bien été mis à jour";
             }
+            header('location: /account');
         }
     }
 }
