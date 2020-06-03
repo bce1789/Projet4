cf7fbafffddd808af0fb2de76cd2bafeed8548da
Benoit Coste <bce.1789@gmail.com>
2020-06-03 09:37:34 +0200

    update spece in responsive mode

--- views/account.php
+++ views/account.php
@@ -6,6 +6,8 @@ ob_start(); ?>
         <p><br></p>
     </div>
     <h1>Bonjour <?= $_SESSION['auth']->username; ?></h1>
+    <p><br><br></p>
+    
     <form action="" method="post">
         <div class="form-group">
             <input class="form-control" type="password" name="password" placeholder="Changer de mot de passe" />
@@ -16,5 +18,6 @@ ob_start(); ?>
         <button class="btn btn-primary">Changer mon mot de passe</button>
     </form>
 </div>
+<div class="space_empty"></div>
 <?php $content = ob_get_clean(); ?>
 <?php require('views/template.php'); ?>
